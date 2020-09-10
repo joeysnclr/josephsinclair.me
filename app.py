4bf6a327a7cdd51c11ffc967b5b702e717c9599e
Joseph Sinclair <joey.sinclair02@gmail.com>
2020-09-10 12:18:48 -0700

    removed github api

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 import os
-import github
 
 app = Flask(__name__)
 port = int(os.environ.get('PORT', 5000))
@@ -8,10 +7,7 @@ port = int(os.environ.get('PORT', 5000))
 
 @app.route('/')
 def index():
-    repos = github.getReposCount()
-    stars = github.getStarCount()
-    contributions = github.getContributionsCount()
-    return render_template('index.html', repos=repos, stars=stars, contributions=contributions)
+    return render_template('index.html')
 
 
 app.run(host='0.0.0.0', port=port, debug=True)
